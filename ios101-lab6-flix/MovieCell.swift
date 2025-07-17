e24801ba6f56707c2d697761a20a4f0689d00858
Trust-Worthy <jondeveloper0@gmail.com>
2025-07-16 19:26:57 -0600

    Working on the navigation version of flix feed.

--- ios101-lab6-flix/MovieCell.swift
+++ ios101-lab6-flix/MovieCell.swift
@@ -1,18 +1,17 @@
 //
 //  MovieCell.swift
-//  ios101-lab5-flix1
-//
-//  Created by Jonathan Bateman on 7/11/25.
+//  ios101-lab-flix
 //
 
 import UIKit
 
-class MovieCell: UITableViewCell{
+class MovieCell: UITableViewCell {
+
 
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- ios101-lab6-flix/ViewController.swift
+++ ios101-lab6-flix/ViewController.swift
@@ -1,68 +1,68 @@
 //
 //  ViewController.swift
-//  ios101-lab5-flix1
+//  ios101-lab6-flix
 //
 
 import UIKit
 import NukeExtensions
 
-// TODO: Add table view data source conformance
+// Conform to UITableViewDataSource
 class ViewController: UIViewController, UITableViewDataSource {
+
+    // MARK: - Table view data source method: numberOfRowsInSection
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // return the number of rows for the table
         print("🍏 numberOfRowsInSection called with movies count: \(movies.count)")
+
+        // Return the number of rows for the table.
         return movies.count
     }
-    
+
+    // MARK: - Table view data source method: cellForRowAt indexPath
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Create, configure, and return a table view cell for the given row (i.e., `indexPath.row`)
-        
-        // get a reusable cell
-        // returns a reusable table-view cell object for the specified reuse identifier and adds it to the table.
-        // This helps optimize table view performance as the app only needs to create enough cells to fill the screen and reuse cells that scroll off the screen instead of creating new ones.
-        // the identifier references the identifier I set for the cell previously in the storyboard
-        // The `dequeueReusableCell` method returns a regular `UITableViewCell`, so I have to cast it to my custom cell (as! MovieCell) to access the custom properties
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell",for: indexPath) as! MovieCell // typecast --> forced it... not good practice
-        
-        
-        // Get the row where the cell will be placed using the 'row' property on the passed in 'indexPath
-        // get the movie-associated table view row
+        // Create, configure and return a table view cell for the given row (i.e. `indexPath.row`)
+
+        print("🍏 cellForRowAt called for row: \(indexPath.row)")
+
+        // Get a reusable cell
+        // Returns a reusable table-view cell object for the specified reuse identifier and adds it to the table. This helps to optimize table view performance as the app only needs to create enough cells to fill the screen and can reuse cells that scroll off the screen instead of creating new ones.
+        // The identifier references the identifier you set for the cell previously in the storyboard.
+        // The `dequeueReusableCell` method returns a regular `UITableViewCell` so we need to cast it as our custom cell (i.e. `as! MovieCell`) in order to access the custom properties you added to the cell.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+
+        // Get the movie associated table view row
         let movie = movies[indexPath.row]
-        
-        // Configure the cell: update UI elements like labels, image views , etc
-        
-        // unwrap the optional poster path
-        if let posterPath = movie.poster_path,  let imageUrl = URL(string: "https://image.tmdb.org/t/p/w500" + posterPath) {
-            
-            // use the NukeExtensions library's load image function to (async) fetch and load the image URL
+
+        // Configure the cell (i.e. update UI elements like labels, image views, etc.)
+
+        // Unwrap the optional poster path
+        if let posterPath = movie.posterPath,
+
+            // Create a url by appending the poster path to the base url. https://developers.themoviedb.org/3/getting-started/images
+           let imageUrl = URL(string: "https://image.tmdb.org/t/p/w500" + posterPath) {
+
+            // Use the Nuke library's load image function to (async) fetch and load the image from the image url.
             NukeExtensions.loadImage(with: imageUrl, into: cell.posterImageView)
         }
-        
-        // set the text on the labels
+
+        // Set the text on the labels
         cell.titleLabel.text = movie.title
         cell.overviewLabel.text = movie.overview
-        
-        print("🍏 cellForRowAt called for row: \(indexPath.row)")
+
+        // Return the cell for use in the respective table view row
         return cell
-        
     }
-    
 
 
-    // TODO: Add table view outlet
+    // Table view outlet
     @IBOutlet weak var tableView: UITableView!
-    
 
-    // TODO: Add property to store fetched movies array
+    // Property to store fetched movies array
     private var movies: [Movie] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // TODO: Assign table view data source
-        // The view controller will act as the tableViews data source
-        // the data source tells the table view how many cells to display
-        // the data source provides the cell UI to use in each table view row
+        // Assign table view data source
         tableView.dataSource = self
 
         fetchMovies()
@@ -74,11 +74,13 @@ class ViewController: UIViewController, UITableViewDataSource {
         // URL for the TMDB Get Popular movies endpoint: https://developers.themoviedb.org/3/movies/get-popular-movies
         let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=b1446bbf3b4c705c6d35e7c67f59c413&language=en-US&page=1")!
 
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData)
+
         // ---
         // Create the URL Session to execute a network request given the above url in order to fetch our movie data.
         // https://developer.apple.com/documentation/foundation/url_loading_system/fetching_website_data_into_memory
         // ---
-        let session = URLSession.shared.dataTask(with: url) { data, response, error in
+        let session = URLSession.shared.dataTask(with: request) { data, response, error in
 
             // Check for errors
             if let error = error {
@@ -101,9 +103,20 @@ class ViewController: UIViewController, UITableViewDataSource {
 
             // The JSONDecoder's decode function can throw an error. To handle any errors we can wrap it in a `do catch` block.
             do {
+                // MARK: - jSONDecoder with custom date formatter
+                let decoder = JSONDecoder()
 
-                // Decode the JSON data into our custom `MovieResponse` model.
-                let movieResponse = try JSONDecoder().decode(MovieResponse.self, from: data)
+                // Create a date formatter object
+                let dateFormatter = DateFormatter()
+
+                // Set the date formatter date format to match the the format of the date string we're trying to parse
+                dateFormatter.dateFormat = "yyyy-MM-dd"
+
+                // Tell the json decoder to use the custom date formatter when decoding dates
+                decoder.dateDecodingStrategy = .formatted(dateFormatter)
+
+                // Decode the JSON data into our custom `MovieFeed` model.
+                let movieResponse = try decoder.decode(MovieFeed.self, from: data)
 
                 // Access the array of movies
                 let movies = movieResponse.results
@@ -113,19 +126,20 @@ class ViewController: UIViewController, UITableViewDataSource {
 
                     // We have movies! Do something with them!
                     print("✅ SUCCESS!!! Fetched \(movies.count) movies")
-                    
+
                     // Iterate over all movies and print out their details.
-                    for movie in movies {
-                        print("🍿 MOVIE ------------------")
+                    for (index, movie) in movies.enumerated() {
+                        print("🍿 MOVIE \(index) ------------------")
                         print("Title: \(movie.title)")
                         print("Overview: \(movie.overview)")
                     }
 
-                    // TODO: Store movies in the `movies` property on the view controller
+                    // Update the movies property so we can access movie data anywhere in the view controller.
                     self?.movies = movies
                     print("🍏 Fetched and stored \(movies.count) movies")
-                    self?.tableView.reloadData()
 
+                    // Prompt the table view to reload its data (i.e. call the data source methods again and re-render contents)
+                    self?.tableView.reloadData()
                 }
             } catch {
                 print("🚨 Error decoding JSON data into Movie Response: \(error.localizedDescription)")
@@ -136,6 +150,4 @@ class ViewController: UIViewController, UITableViewDataSource {
         // Don't forget to run the session!
         session.resume()
     }
-
-
 }
